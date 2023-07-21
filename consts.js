e6ab55074d830bdeaf904bfaf17a399c4ee28bd4
fabrisdev <ezepuntocom@hotmail.com>
2023-07-21 16:31:25 -0300

    Constants changed to make images bigger

--- consts.js
+++ consts.js
@@ -2,5 +2,5 @@ import path from 'node:path'
 
 export const ASCII_OUTPUT_FILE_PATH = path.join("images", "ascii.txt")
 export const GAP = 10
-export const IMAGE_SIZE = 20
+export const IMAGE_SIZE = 30
 export const IMAGE_HORIZONTAL_SCALE = 2
