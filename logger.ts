9cdbc1aebf322e22d0532552c6461fde838cb21f
fabrisdev <ezepuntocom@hotmail.com>
2026-01-02 22:25:33 -0300

    Added method chaining

--- logger.ts
+++ logger.ts
@@ -3,8 +3,16 @@ import chalk from "chalk";
 class Logger {
 	success(message: string) {
 		console.log(chalk.green(message));
+		return this as Omit<Logger, "crash">;
 	}
 	error(message: string) {
 		console.error(chalk.red(message));
+		return this as Omit<Logger, "exit">;
+	}
+	crash() {
+		process.exit(1);
+	}
+	exit() {
+		process.exit(0);
 	}
 }
