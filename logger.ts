0922777525ff58b3093220ffefb6d576ceea7437
fabrisdev <ezepuntocom@hotmail.com>
2026-01-02 22:28:06 -0300

    rewrote it as an object

--- logger.ts
+++ logger.ts
@@ -1,18 +1,18 @@
 import chalk from "chalk";
 
-class Logger {
+const log = {
 	success(message: string) {
 		console.log(chalk.green(message));
-		return this as Omit<Logger, "crash">;
-	}
+		return this as Omit<typeof log, "crash">;
+	},
 	error(message: string) {
 		console.error(chalk.red(message));
-		return this as Omit<Logger, "exit">;
-	}
+		return this as Omit<typeof log, "exit">;
+	},
 	crash() {
 		process.exit(1);
-	}
+	},
 	exit() {
 		process.exit(0);
-	}
-}
+	},
+};
