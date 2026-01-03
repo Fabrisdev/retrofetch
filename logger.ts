bd527007ee9477abcef362257eb41dbc606353ed
fabrisdev <ezepuntocom@hotmail.com>
2026-01-02 22:29:59 -0300

    Made it so you can only use success or crash on the first call

--- logger.ts
+++ logger.ts
@@ -1,18 +1,21 @@
 import chalk from "chalk";
 
-const log = {
-	success(message: string) {
-		console.log(chalk.green(message));
-		return this as Omit<typeof log, "crash">;
-	},
-	error(message: string) {
-		console.error(chalk.red(message));
-		return this as Omit<typeof log, "exit">;
-	},
-	crash() {
-		process.exit(1);
-	},
-	exit() {
-		process.exit(0);
-	},
-};
+function log() {
+	const logger = {
+		success(message: string) {
+			console.log(chalk.green(message));
+			return this as Omit<typeof log, "crash">;
+		},
+		error(message: string) {
+			console.error(chalk.red(message));
+			return this as Omit<typeof log, "exit">;
+		},
+		crash() {
+			process.exit(1);
+		},
+		exit() {
+			process.exit(0);
+		},
+	};
+	return logger as Pick<typeof logger, "success" | "crash">;
+}
