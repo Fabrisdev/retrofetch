d0a135f06639a008494ba9ea2ebf51d13d872e31
fabrisdev <ezepuntocom@hotmail.com>
2026-01-04 00:52:58 -0300

    Changed path to .config folder

--- consts.js
+++ consts.js
@@ -1,6 +1,12 @@
+import { homedir } from "node:os";
 import path from "node:path";
 
-export const ASCII_OUTPUT_FILE_PATH = path.join("images", "ascii.txt");
+export const ASCII_OUTPUT_FILE_PATH = path.join(
+	homedir(),
+	".config",
+	"retrofetch",
+	"ascii.txt",
+);
 export const GAP = 10;
 export const IMAGE_SIZE = 30;
 export const IMAGE_HORIZONTAL_SCALE = 2;
